2cc2ce512d342496293518b4b09e340aed07cc16
alfrebur <alfre.burgue@gmail.com>
2022-03-04 17:50:09 -0300

    agregada la base de datos

---
+++ docs/wineline.sql
@@ -0,0 +1,276 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 04-03-2022 a las 21:24:33
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `wineline`
+--
+CREATE DATABASE IF NOT EXISTS `wineline` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `wineline`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `brands`
+--
+
+DROP TABLE IF EXISTS `brands`;
+CREATE TABLE `brands` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cartproduct`
+--
+
+DROP TABLE IF EXISTS `cartproduct`;
+CREATE TABLE `cartproduct` (
+  `id` int(11) NOT NULL,
+  `cartId` int(11) NOT NULL,
+  `productId` int(11) NOT NULL,
+  `subtotal` decimal(6,2) NOT NULL,
+  `price` int(11) NOT NULL,
+  `discount` decimal(2,1) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `order_date` date NOT NULL,
+  `order_ship` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `carts`
+--
+
+DROP TABLE IF EXISTS `carts`;
+CREATE TABLE `carts` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `userId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE `products` (
+  `id` int(20) NOT NULL,
+  `brand` varchar(100) NOT NULL,
+  `variety` varchar(100) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `price` decimal(6,2) NOT NULL,
+  `volume` decimal(4,2) NOT NULL,
+  `category` varchar(100) NOT NULL,
+  `description` text NOT NULL,
+  `discount` decimal(3,2) NOT NULL,
+  `new` bit(2) NOT NULL,
+  `create_at` date NOT NULL,
+  `modified_at` date NOT NULL,
+  `delete_at` date NOT NULL,
+  `brandId` int(100) NOT NULL,
+  `categoryId` int(100) NOT NULL,
+  `varietyId` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `userName` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `country` varchar(100) NOT NULL,
+  `adress` varchar(100) NOT NULL,
+  `avatar` varchar(100) NOT NULL,
+  `admin` bit(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `varieties`
+--
+
+DROP TABLE IF EXISTS `varieties`;
+CREATE TABLE `varieties` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `volumeproduct`
+--
+
+DROP TABLE IF EXISTS `volumeproduct`;
+CREATE TABLE `volumeproduct` (
+  `id` int(11) NOT NULL,
+  `productId` int(11) NOT NULL,
+  `volumeId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `volumes`
+--
+
+DROP TABLE IF EXISTS `volumes`;
+CREATE TABLE `volumes` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `brands`
+--
+ALTER TABLE `brands`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `cartproduct`
+--
+ALTER TABLE `cartproduct`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `cartId` (`cartId`),
+  ADD KEY `productId` (`productId`);
+
+--
+-- Indices de la tabla `carts`
+--
+ALTER TABLE `carts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- Indices de la tabla `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `brandId` (`brandId`),
+  ADD KEY `categoryId` (`categoryId`),
+  ADD KEY `varietyId` (`varietyId`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `userEmail` (`email`);
+
+--
+-- Indices de la tabla `varieties`
+--
+ALTER TABLE `varieties`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `volumeproduct`
+--
+ALTER TABLE `volumeproduct`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `productId` (`productId`),
+  ADD KEY `volumeId` (`volumeId`);
+
+--
+-- Indices de la tabla `volumes`
+--
+ALTER TABLE `volumes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `volumeproduct`
+--
+ALTER TABLE `volumeproduct`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `cartproduct`
+--
+ALTER TABLE `cartproduct`
+  ADD CONSTRAINT `cartproduct_ibfk_1` FOREIGN KEY (`cartId`) REFERENCES `carts` (`id`),
+  ADD CONSTRAINT `cartproduct_ibfk_2` FOREIGN KEY (`productId`) REFERENCES `products` (`id`);
+
+--
+-- Filtros para la tabla `carts`
+--
+ALTER TABLE `carts`
+  ADD CONSTRAINT `carts_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
+
+--
+-- Filtros para la tabla `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`brandId`) REFERENCES `brands` (`id`),
+  ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`categoryId`) REFERENCES `categories` (`id`),
+  ADD CONSTRAINT `products_ibfk_3` FOREIGN KEY (`varietyId`) REFERENCES `varieties` (`id`);
+
+--
+-- Filtros para la tabla `volumeproduct`
+--
+ALTER TABLE `volumeproduct`
+  ADD CONSTRAINT `volumeproduct_ibfk_1` FOREIGN KEY (`productId`) REFERENCES `products` (`id`),
+  ADD CONSTRAINT `volumeproduct_ibfk_2` FOREIGN KEY (`volumeId`) REFERENCES `volumes` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
