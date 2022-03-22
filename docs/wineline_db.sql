f85f2dee927ac7cbdd820f5816f15c0f8ca03896
Nahuel7709 <nahuelmenichetti98@gmail.com>
2022-03-21 22:57:01 -0300

    ajuste database

--- docs/wineline_db.sql
+++ docs/wineline_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-03-2022 a las 22:32:44
+-- Tiempo de generación: 22-03-2022 a las 02:53:18
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.2
 
@@ -33,9 +33,16 @@ DROP TABLE IF EXISTS `brands`;
 CREATE TABLE `brands` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(100) NOT NULL,
-  `createAt` timestamp NULL DEFAULT NULL,
-  `modiefiedAt` timestamp NULL DEFAULT NULL
+  `createdAt` timestamp NULL DEFAULT NULL,
+  `updatedAt` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `brands`
+--
+
+INSERT INTO `brands` (`id`, `name`, `createdAt`, `updatedAt`) VALUES
+(1, 'Trumpeter', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -47,15 +54,15 @@ DROP TABLE IF EXISTS `cartproduct`;
 CREATE TABLE `cartproduct` (
   `id` int(10) UNSIGNED NOT NULL,
   `subtotal` decimal(6,2) NOT NULL,
-  `price` int(11) NOT NULL,
+  `price` decimal(6,2) NOT NULL,
   `discount` decimal(2,1) NOT NULL,
   `quantity` int(11) NOT NULL,
   `order_date` date NOT NULL,
   `order_ship` date NOT NULL,
   `cartId` int(10) UNSIGNED DEFAULT NULL,
   `productId` int(10) UNSIGNED DEFAULT NULL,
-  `createAt` timestamp NULL DEFAULT NULL,
-  `modiefiedAt` timestamp NULL DEFAULT NULL
+  `createdAt` timestamp NULL DEFAULT NULL,
+  `updatedAt` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -69,8 +76,8 @@ CREATE TABLE `carts` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(100) NOT NULL,
   `userId` int(10) UNSIGNED DEFAULT NULL,
-  `createAt` timestamp NULL DEFAULT NULL,
-  `modiefiedAt` timestamp NULL DEFAULT NULL
+  `createdAt` timestamp NULL DEFAULT NULL,
+  `updatedAt` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -83,9 +90,18 @@ DROP TABLE IF EXISTS `categories`;
 CREATE TABLE `categories` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(100) NOT NULL,
-  `createAt` timestamp NULL DEFAULT NULL,
-  `modiefiedAt` timestamp NULL DEFAULT NULL
+  `createdAt` timestamp NULL DEFAULT NULL,
+  `updatedAt` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `createdAt`, `updatedAt`) VALUES
+(1, 'Vinos', NULL, NULL),
+(2, 'Cervezas', NULL, NULL),
+(3, 'Espumantes', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -96,19 +112,28 @@ CREATE TABLE `categories` (
 DROP TABLE IF EXISTS `products`;
 CREATE TABLE `products` (
   `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
   `image` varchar(100) NOT NULL,
   `price` decimal(6,2) NOT NULL,
-  `volume` decimal(4,2) NOT NULL,
   `description` text NOT NULL,
-  `discount` decimal(3,2) NOT NULL,
+  `discount` decimal(5,2) NOT NULL,
   `new` bit(2) NOT NULL,
-  `createAt` date DEFAULT NULL,
-  `modifiedAt` date DEFAULT NULL,
-  `deleteAt` date DEFAULT NULL,
+  `createdAt` timestamp NULL DEFAULT NULL,
+  `updatedAt` timestamp NULL DEFAULT NULL,
+  `deletedAt` timestamp NULL DEFAULT NULL,
   `brandId` int(10) UNSIGNED DEFAULT NULL,
   `categoryId` int(10) UNSIGNED DEFAULT NULL,
-  `varietyId` int(10) UNSIGNED DEFAULT NULL
+  `varietyId` int(10) UNSIGNED DEFAULT NULL,
+  `volumeId` int(10) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `image`, `price`, `description`, `discount`, `new`, `createdAt`, `updatedAt`, `deletedAt`, `brandId`, `categoryId`, `varietyId`, `volumeId`) VALUES
+(1, 'Trumpeter', '1643036550919-637800597_productImage.jpg', '1160.00', 'Rojo violáceo brillante, seduce con sus aromas frutales (ciruela) y especiados (canela, cardamomo, pimienta negra). Posee gran cuerpo y su vivaz estructura acentúa los taninos intensos que se vuelven aterciopelados en el retrogusto. Variedad: 100% Malbec.', '0.00', b'00', NULL, NULL, NULL, 1, 1, 1, 1),
+(22, 'Luigi Bosca', '1647906268559-627256211_image.jpg', '1350.00', 'Luigi Bosca Malbec es un tinto de color rojo violáceo brillante. Sus aromas son intensos y amables, con notas que recuerdan a frutas rojas, y tonos algo florales y especiados. En boca es generoso, fluido y expresivo, con taninos incipientes. De paladar franco y paso refrescante, con buen cuerpo y carácter.', '10.00', b'00', '2022-03-21 23:44:28', '2022-03-21 23:44:28', NULL, NULL, 1, 3, 1);
 
 -- --------------------------------------------------------
 
@@ -120,16 +145,26 @@ DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(255) NOT NULL,
-  `userName` int(255) NOT NULL,
+  `userName` varchar(30) NOT NULL,
   `email` varchar(255) NOT NULL,
+  `cellphone` varchar(20) NOT NULL,
   `password` varchar(255) NOT NULL,
   `country` varchar(100) NOT NULL,
-  `adress` varchar(255) NOT NULL,
+  `adress` text NOT NULL,
   `avatar` varchar(255) NOT NULL,
   `admin` bit(1) NOT NULL,
-  `createAt` timestamp NULL DEFAULT NULL,
-  `modifiedAt` timestamp NULL DEFAULT NULL
+  `createdAt` timestamp NULL DEFAULT NULL,
+  `updatedAt` timestamp NULL DEFAULT NULL,
+  `deletedAt` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `userName`, `email`, `cellphone`, `password`, `country`, `adress`, `avatar`, `admin`, `createdAt`, `updatedAt`, `deletedAt`) VALUES
+(1, 'Nahuel', 'Nahuel09', 'nahuelm@gmail.com', '111111111111', '$2a$10$4dCro16iMPtCS4lOgxS8HeFlVajITSLwfCGJjutwa7H8OuDHcHyam', 'Argentina', 'Calle Falsa 123', '1647532871978-416983657_avatar.jpg', b'0', '2022-03-17 16:01:12', '2022-03-17 16:01:12', NULL),
+(2, 'Nahuel Prueba', 'Prueba123', 'usuarioprueba@gmail.com', '111112211111', '$2a$10$zBcqzaSD16ANOPGKzCNuRe6NTHbLtGwJUEQ7TFG/jk7bzPK.ecCoq', 'Argentina', 'Calle Falsa 700', '1647538940166-935557699_avatar.jpg', b'0', '2022-03-17 17:42:20', '2022-03-17 17:42:20', NULL);
 
 -- --------------------------------------------------------
 
@@ -141,24 +176,24 @@ DROP TABLE IF EXISTS `varieties`;
 CREATE TABLE `varieties` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(100) NOT NULL,
-  `createAt` timestamp NULL DEFAULT NULL,
-  `modiefiedAt` timestamp NULL DEFAULT NULL
+  `createdAt` timestamp NULL DEFAULT NULL,
+  `updatedAt` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
 --
--- Estructura de tabla para la tabla `volumeproduct`
+-- Volcado de datos para la tabla `varieties`
 --
 
-DROP TABLE IF EXISTS `volumeproduct`;
-CREATE TABLE `volumeproduct` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `productId` int(10) UNSIGNED NOT NULL,
-  `volumeId` int(10) UNSIGNED NOT NULL,
-  `createAt` timestamp NULL DEFAULT NULL,
-  `modiefiedAt` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `varieties` (`id`, `name`, `createdAt`, `updatedAt`) VALUES
+(1, 'Malbec', NULL, NULL),
+(2, 'Cavernet', NULL, NULL),
+(3, 'Cabernet Sauvignon', NULL, NULL),
+(4, 'Merlot', NULL, NULL),
+(5, 'Pinot Noir', NULL, NULL),
+(6, 'Brut', NULL, NULL),
+(7, 'Rosé ', NULL, NULL),
+(8, 'Extra Brut', NULL, NULL),
+(9, 'Blanc de noirs', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -170,9 +205,21 @@ DROP TABLE IF EXISTS `volumes`;
 CREATE TABLE `volumes` (
   `id` int(10) UNSIGNED NOT NULL,
   `name` varchar(100) NOT NULL,
-  `createAt` timestamp NULL DEFAULT NULL,
-  `modiefiedAt` timestamp NULL DEFAULT NULL
+  `createdAt` timestamp NULL DEFAULT NULL,
+  `updatedAt` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `volumes`
+--
+
+INSERT INTO `volumes` (`id`, `name`, `createdAt`, `updatedAt`) VALUES
+(1, '750', NULL, NULL),
+(2, '200', NULL, NULL),
+(3, '250', NULL, NULL),
+(4, '375', NULL, NULL),
+(5, '1500', NULL, NULL),
+(6, '3000', NULL, NULL);
 
 --
 -- Índices para tablas volcadas
@@ -212,7 +259,8 @@ ALTER TABLE `products`
   ADD PRIMARY KEY (`id`),
   ADD KEY `brandId` (`brandId`),
   ADD KEY `varietyId` (`varietyId`),
-  ADD KEY `categoryId` (`categoryId`);
+  ADD KEY `categoryId` (`categoryId`),
+  ADD KEY `volumeId` (`volumeId`);
 
 --
 -- Indices de la tabla `users`
@@ -225,14 +273,6 @@ ALTER TABLE `users`
 --
 ALTER TABLE `varieties`
   ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `volumeproduct`
---
-ALTER TABLE `volumeproduct`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `productId` (`productId`),
-  ADD KEY `volumeId` (`volumeId`);
 
 --
 -- Indices de la tabla `volumes`
@@ -248,7 +288,7 @@ ALTER TABLE `volumes`
 -- AUTO_INCREMENT de la tabla `brands`
 --
 ALTER TABLE `brands`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `cartproduct`
@@ -266,37 +306,31 @@ ALTER TABLE `carts`
 -- AUTO_INCREMENT de la tabla `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `varieties`
 --
 ALTER TABLE `varieties`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `volumeproduct`
---
-ALTER TABLE `volumeproduct`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `volumes`
 --
 ALTER TABLE `volumes`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Restricciones para tablas volcadas
@@ -321,14 +355,8 @@ ALTER TABLE `carts`
 ALTER TABLE `products`
   ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`brandId`) REFERENCES `brands` (`id`),
   ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`varietyId`) REFERENCES `varieties` (`id`),
-  ADD CONSTRAINT `products_ibfk_3` FOREIGN KEY (`categoryId`) REFERENCES `categories` (`id`);
-
---
--- Filtros para la tabla `volumeproduct`
---
-ALTER TABLE `volumeproduct`
-  ADD CONSTRAINT `volumeproduct_ibfk_1` FOREIGN KEY (`productId`) REFERENCES `products` (`id`),
-  ADD CONSTRAINT `volumeproduct_ibfk_2` FOREIGN KEY (`volumeId`) REFERENCES `volumes` (`id`);
+  ADD CONSTRAINT `products_ibfk_3` FOREIGN KEY (`categoryId`) REFERENCES `categories` (`id`),
+  ADD CONSTRAINT `products_ibfk_4` FOREIGN KEY (`volumeId`) REFERENCES `volumes` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
