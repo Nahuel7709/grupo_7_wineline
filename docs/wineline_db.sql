eb9878d742f4c1583e32d74f0b94abb6aca1528f
Nahuel7709 <nahuelmenichetti98@gmail.com>
2022-03-23 17:56:08 -0300

    ajuste database

--- docs/wineline_db.sql
+++ docs/wineline_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-03-2022 a las 02:53:18
+-- Tiempo de generación: 23-03-2022 a las 21:54:55
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.2
 
@@ -133,7 +133,14 @@ CREATE TABLE `products` (
 
 INSERT INTO `products` (`id`, `name`, `image`, `price`, `description`, `discount`, `new`, `createdAt`, `updatedAt`, `deletedAt`, `brandId`, `categoryId`, `varietyId`, `volumeId`) VALUES
 (1, 'Trumpeter', '1643036550919-637800597_productImage.jpg', '1160.00', 'Rojo violáceo brillante, seduce con sus aromas frutales (ciruela) y especiados (canela, cardamomo, pimienta negra). Posee gran cuerpo y su vivaz estructura acentúa los taninos intensos que se vuelven aterciopelados en el retrogusto. Variedad: 100% Malbec.', '0.00', b'00', NULL, NULL, NULL, 1, 1, 1, 1),
-(22, 'Luigi Bosca', '1647906268559-627256211_image.jpg', '1350.00', 'Luigi Bosca Malbec es un tinto de color rojo violáceo brillante. Sus aromas son intensos y amables, con notas que recuerdan a frutas rojas, y tonos algo florales y especiados. En boca es generoso, fluido y expresivo, con taninos incipientes. De paladar franco y paso refrescante, con buen cuerpo y carácter.', '10.00', b'00', '2022-03-21 23:44:28', '2022-03-21 23:44:28', NULL, NULL, 1, 3, 1);
+(22, 'Luigi Bosca', '1648056980620-7267540_image.jpg', '1350.00', 'Luigi Bosca Malbec es un tinto de color rojo violáceo brillante. Sus aromas son intensos y amables, con notas que recuerdan a frutas rojas, y tonos algo florales y especiados. En boca es generoso, fluido y expresivo, con taninos incipientes. De paladar franco y paso refrescante, con buen cuerpo y carácter.', '10.00', b'00', '2022-03-21 23:44:28', '2022-03-23 17:36:20', NULL, NULL, 1, 1, 1),
+(23, 'Baron B', '1647914157910-710979778_image.jpg', '2200.00', 'Sus finas y persistentes burbujas, de color amarillo con destellos ocres, revelan autenticidad y delicadeza. Aromas expresivos a damasco y frutos ci´tricos se combinan con notas de pan tostado y frutos secos, dando complejidad a este espumante con final de boca sensual y de gran persistencia.', '0.00', b'00', '2022-03-22 01:55:57', '2022-03-22 01:55:57', NULL, NULL, 3, 8, 5),
+(24, 'Nieto Senetiner', '1648056430026-621253868_image.jpg', '1100.00', 'Ligero tono ámbar. Aroma complejo y frutado, recuerda notas de frambuesa junto con notas de praliné y pan tostado, fiel a la excelencia del Pinot Noir.', '5.00', b'00', '2022-03-23 17:27:10', '2022-03-23 17:27:10', NULL, NULL, 3, 8, 1),
+(25, 'Rutini', '1648056488981-474713934_image.jpg', '1215.00', 'Rojo intenso, con matices azulados. En nariz, se presenta frutado, con notas de ciruela, vainilla y anís; mientras, en boca, se reafirman los acentos aciruelados. Los taninos, muy presentes pero amables, destacan su personalidad. Variedad: 100% Malbec de Altamira, Gualtallary y La Consulta.', '0.00', b'00', '2022-03-23 17:28:08', '2022-03-23 17:28:08', NULL, NULL, 1, 1, 1),
+(26, 'Stella Artois', '1648056698740-950470696_image.png', '160.00', 'Tiene un Sabor sutil, proviene de la mezcla de la mejor malta y las mejores variedades de lúpulo. Utilizando sólo ingredientes naturales que garantiza una lager fresca, de alta calidad con un ligero sabor amargo.', '0.00', b'00', '2022-03-23 17:31:38', '2022-03-23 17:31:38', NULL, NULL, 2, 10, 4),
+(27, 'Corona', '1648056768725-537134649_image.jpg', '220.00', 'Corona Porron 710 cc (Pack x 12) Corona Extra es única porque tiene un color inconfundible, un sabor incomparable y en definitiva no tiene igual. ... Corona es una cerveza clara que no esconde nada, de ahí su botella transparente. Y es que cuando se usan sólo los mejores ingredientes, no hay nada que ocultar.', '0.00', b'00', '2022-03-23 17:32:48', '2022-03-23 17:32:48', NULL, NULL, 2, 10, 4),
+(28, 'Chandon', '1648056853493-692616796_image.jpg', '990.00', 'Chandon Délice es la sutil variante de un estilo. Versátil por combinar lo clásico del Chardonnay y Pinot Noir con lo vanguardista del Sémillon y Petit Manseng. Este espumante traspasa la frontera de sabores. Su color es amarillo brillante con algunos matices verdosos.', '0.00', b'00', '2022-03-23 17:34:13', '2022-03-23 17:34:13', NULL, NULL, 3, 11, 1),
+(29, 'Andes', '1648056904484-16983656_image.jpg', '390.00', 'La Andes Origen Rubia es una Cerveza estilo american pilsner, una Rubia con cuerpo y carácter maltoso. Aroma leve a lúpulo, cereal y toque de levadura. Debe servirse en copa para apreciar mejor sus aromas y resaltar así su color dorado brillante.', '0.00', b'00', '2022-03-23 17:35:04', '2022-03-23 17:35:04', NULL, NULL, 2, 10, 1);
 
 -- --------------------------------------------------------
 
@@ -163,8 +170,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `userName`, `email`, `cellphone`, `password`, `country`, `adress`, `avatar`, `admin`, `createdAt`, `updatedAt`, `deletedAt`) VALUES
-(1, 'Nahuel', 'Nahuel09', 'nahuelm@gmail.com', '111111111111', '$2a$10$4dCro16iMPtCS4lOgxS8HeFlVajITSLwfCGJjutwa7H8OuDHcHyam', 'Argentina', 'Calle Falsa 123', '1647532871978-416983657_avatar.jpg', b'0', '2022-03-17 16:01:12', '2022-03-17 16:01:12', NULL),
-(2, 'Nahuel Prueba', 'Prueba123', 'usuarioprueba@gmail.com', '111112211111', '$2a$10$zBcqzaSD16ANOPGKzCNuRe6NTHbLtGwJUEQ7TFG/jk7bzPK.ecCoq', 'Argentina', 'Calle Falsa 700', '1647538940166-935557699_avatar.jpg', b'0', '2022-03-17 17:42:20', '2022-03-17 17:42:20', NULL);
+(1, 'Nahuel', 'Nahuel09', 'nahuelm@gmail.com', '111111111111', '$2a$10$ICUYIZxNudrwf/2XVSPyNOVl1uFnfugFwluivca6Gs86vrbqM5YEC', 'Uruguay', 'Calle Falsa 123', '1648057629280-787712398_avatar.jpg', b'0', '2022-03-17 16:01:12', '2022-03-23 17:47:09', NULL),
+(2, 'Nahuel Prueba', 'PruebaActualiz', 'usuarioprueba@gmail.com', '111112211111', '$2a$10$DlYlVxSUVWZsH5PZEmPeuuMP4/ePlPVMe1tsDSOI9UYVrV8V6JnQu', 'Venezuela', 'Calle Falsa 700', '1648057395550-73002422_avatar.jpg', b'0', '2022-03-17 17:42:20', '2022-03-23 17:43:15', NULL);
 
 -- --------------------------------------------------------
 
@@ -193,7 +200,9 @@ INSERT INTO `varieties` (`id`, `name`, `createdAt`, `updatedAt`) VALUES
 (6, 'Brut', NULL, NULL),
 (7, 'Rosé ', NULL, NULL),
 (8, 'Extra Brut', NULL, NULL),
-(9, 'Blanc de noirs', NULL, NULL);
+(9, 'Blanc de noirs', NULL, NULL),
+(10, 'Rubia', NULL, NULL),
+(11, 'Delice', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -312,7 +321,7 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT de la tabla `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
@@ -324,7 +333,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `varieties`
 --
 ALTER TABLE `varieties`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de la tabla `volumes`
